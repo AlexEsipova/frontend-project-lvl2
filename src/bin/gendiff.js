1dee91bec2ee35d304d5f45e5427b3e32b2fe02b
Sasha Esipova <89099438452@mail.ru>
2020-09-17 21:22:22 +0300

    add linter

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,8 +3,9 @@
 import program from 'commander';
 
 program
-    .version('1.0.0', '-V, --version', 'output the version number')
-    .description('Compares two configuration files and shows a difference.');
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse(process.argv);
-
