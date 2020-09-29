b61a49386cafee4c8e2e76b590de75991c06897d
Sasha Esipova <89099438452@mail.ru>
2020-09-29 18:20:27 +0300

    add nested objects

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -14,3 +14,12 @@ test.each([
   expect(genDiff(getFixturePath(`before${format}`), getFixturePath(`after${format}`)))
     .toBe((readFile('expected.txt')));
 });
+
+test.each([
+  ['.json'],
+  ['.yml'],
+  ['.ini'],
+])('gendiff', (format) => {
+  expect(genDiff(getFixturePath(`before_structural${format}`), getFixturePath(`after_structural${format}`)))
+    .toBe((readFile('expected_structural.txt')));
+});
--- src/index.js
+++ src/index.js
@@ -2,24 +2,29 @@ import _ from 'lodash';
 import parser from './parsers.js';
 
 const genDiff = (file1, file2) => {
-  const obj1 = parser(file1);
-  const obj2 = parser(file2);
-  const keys = Object.keys({ ...obj1, ...obj2 });
-  const sortedKeys = keys.sort();
-  const result = [];
-  sortedKeys.forEach((key) => {
-    if (!_.has(obj2, key)) {
-      result.push([`- ${key}: ${obj1[key]}`]);
-    } else if (!_.has(obj1, key)) {
-      result.push([`+ ${key}: ${obj2[key]}`]);
-    } else if (obj1[key] === obj2[key]) {
-      result.push([`  ${key}: ${obj1[key]}`]);
-    } else {
-      result.push([`- ${key}: ${obj1[key]}`]);
-      result.push([`+ ${key}: ${obj2[key]}`]);
-    }
-  });
-  return `{\n  ${result.join('\n  ')}\n}`;
+  const newObj1 = parser(file1);
+  const newObj2 = parser(file2);
+  const findDiff = (obj1, obj2) => {
+    const keys = Object.keys({ ...obj1, ...obj2 });
+    const sortedKeys = keys.sort();
+    const result = [];
+    sortedKeys.forEach((key) => {
+      if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+        result.push([`  ${key}: ${findDiff(obj1[key], obj2[key])}`]);
+      } else if (!_.has(obj2, key)) {
+        result.push([`- ${key}: ${obj1[key]}`]);
+      } else if (!_.has(obj1, key)) {
+        result.push([`+ ${key}: ${obj2[key]}`]);
+      } else if (obj1[key] === obj2[key]) {
+        result.push([`  ${key}: ${obj1[key]}`]);
+      } else {
+        result.push([`- ${key}: ${obj1[key]}`]);
+        result.push([`+ ${key}: ${obj2[key]}`]);
+      }
+    });
+    return `{\n  ${result.join('\n  ')}\n}`;
+  };
+  return findDiff(newObj1, newObj2);
 };
 
 export default genDiff;
