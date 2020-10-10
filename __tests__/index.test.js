3c7ab84ce4e7db92ac918d74106a08b6c827d1b6
Sasha Esipova <89099438452@mail.ru>
2020-10-10 13:22:58 +0300

    remove duplication

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,15 +11,6 @@ test.each([
   ['.yml'],
   ['.ini'],
 ])('gendiff', (format) => {
-  expect(genDiff(getFixturePath(`before${format}`), getFixturePath(`after${format}`)))
-    .toBe((readFile('expected.txt')));
-});
-
-test.each([
-  ['.json'],
-  ['.yml'],
-  ['.ini'],
-])('gendiff', (format) => {
   expect(genDiff(getFixturePath(`before_structural${format}`), getFixturePath(`after_structural${format}`)))
     .toBe((readFile('expected_structural.txt')));
 });
