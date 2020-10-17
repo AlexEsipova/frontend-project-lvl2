6bd67f922d5f2fb1a68573c199bc40e0d1fe0efd
Sasha Esipova <89099438452@mail.ru>
2020-10-17 21:32:42 +0300

    fix sort1

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ const genDiff = (file1, file2, format = 'stylish') => {
   const newObj1 = formatParser(file1);
   const newObj2 = formatParser(file2);
   const findDiff = (obj1, obj2) => {
-    const keys = Object.keys({ ...obj1, ...obj2 }).sort();
-    const result = keys.map((key) => {
+    const keys = Object.keys({ ...obj1, ...obj2 });
+    const sortedKeys = _.sortBy(keys);
+    const result = sortedKeys.map((key) => {
       if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
         return [key, { value1: findDiff(obj1[key], obj2[key]), type: 'unchanged' }];
       }
