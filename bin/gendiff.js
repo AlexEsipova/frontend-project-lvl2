0c81572354cd7b7b96e10da4d6630689143f6826
Sasha Esipova <89099438452@mail.ru>
2020-10-30 00:49:48 +0300

    delete reading json

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,11 @@
-#!/usr/bin/node --experimental-json-modules
+#!/usr/bin/env node
 
 import program from 'commander';
 import genDiff from '../src/index.js';
-import pkg from '../package.json';
 
 program
-  .version(pkg.version, '-V, --version', 'output the version number')
-  .description(pkg.description)
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
