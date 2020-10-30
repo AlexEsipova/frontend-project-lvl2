4723963142df14c35db22eccd2cf64291ffc90f0
Sasha Esipova <89099438452@mail.ru>
2020-10-30 12:58:48 +0300

    update tests and read pkg

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,17 +6,12 @@ import genDiff from '../src/index.js';
 const getFixturePath = (filename) => path.join('__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
 
-let formatsAndExpected;
-
-beforeAll(() => {
-  formatsAndExpected = [
+describe.each([['.json'], ['.yml'], ['.ini']])('extension %s', (extention) => {
+  const formatsAndExpected = [
     ['stylish', readFile('expected_stylish.txt')],
     ['plain', readFile('expected_plain.txt')],
     ['json', readFile('expected_json.txt')],
   ];
-});
-
-describe.each([['.json'], ['.yml'], ['.ini']])('extension %s', (extention) => {
   test.each(formatsAndExpected)('gendiff', (format, expected) => {
     expect(genDiff(getFixturePath(`before${extention}`), getFixturePath(`after${extention}`), format))
       .toBe(expected);
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import fs from 'fs';
 import genDiff from '../src/index.js';
 
+const contentOfPKG = JSON.parse(fs.readFileSync('/home/alex_esipova/frontend-project-lvl2/package.json'));
+
 program
-  .version('1.0.0', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.')
+  .version(contentOfPKG.version, '-V, --version', 'output the version number')
+  .description(contentOfPKG.description)
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
